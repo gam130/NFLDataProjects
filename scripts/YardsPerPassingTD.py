8036bb5d7af419987125c4cceb1276371016e88d
Gautam Maybhate <gautam.maybhate@gmail.com>
2020-12-19 13:53:34 -0500

    first commit

---
+++ scripts/YardsPerPassingTD.py
@@ -0,0 +1,29 @@
+import pandas as pd
+import matplotlib.pyplot as plt
+import numpy as np
+
+YEAR = 2019
+
+data = pd.read_csv('https://github.com/guga31bb/nflfastR-data/blob/master/data/' \
+                         'play_by_play_' + str(YEAR) + '.csv.gz?raw=True',
+                         compression='gzip', low_memory=False)
+
+data = data.loc[data.season_type=='REG']
+
+yards = data.yards_gained.loc[(data.play_type=='pass') & (data.complete_pass==1)]
+air_yards = data.air_yards.loc[(data.play_type=='pass') & (data.complete_pass==1)]
+print(air_yards.count())
+plt.figure(figsize=(12, 8))
+
+plt.hist(yards, bins=100, color='blue', label='Total yards per complete pass')
+plt.hist(air_yards, bins=50, color='red', label='Air Yards per complete pass', alpha=.7)
+
+plt.xlabel('Yards per complete pass', fontsize=12)
+plt.ylabel('Number of passes', fontsize=12)
+
+plt.title('Distribution of how long passes were in 2019')
+
+plt.xticks(np.arange(-10, 100, step=10))
+plt.legend()
+
+plt.savefig('figures/ydsTDdist.png', dpi=400)
---
+++ scripts/test.py
@@ -0,0 +1,36 @@
+import pandas as pd
+import matplotlib.pyplot as plt
+
+YEAR = 2019
+
+data = pd.read_csv('https://github.com/guga31bb/nflfastR-data/blob/master/data/' \
+                         'play_by_play_' + str(YEAR) + '.csv.gz?raw=True',
+                         compression='gzip', low_memory=False)
+                         
+data = data.loc[data.season_type=='REG']
+
+passing_tds = data.loc[(data.play_type=='pass') & (data.touchdown==1) & (data.qb_scramble==0) & (data.posteam==data.td_team)] 
+passing_tds = passing_tds.groupby(['passer']).agg({'yards_gained' : 'mean', 'play_id' : 'count'})
+
+passing_tds = passing_tds.loc[passing_tds.play_id > 4]
+
+passing_tds.sort_values('play_id', ascending=False, inplace=True)
+passing_tds.columns = ['Avg. TD yardage', 'TDs']
+
+passing_tds = passing_tds.round(2)
+
+print(passing_tds)
+
+plt.figure(figsize=(10,10))
+
+plt.scatter(passing_tds['Avg. TD yardage'], passing_tds['TDs'], alpha=0.7, color='red')
+
+plt.xlabel('Avg. TD Yardage')
+plt.ylabel('Number of TDs')
+plt.title('Length and Number of Touchdown passes in the NFL by QB')
+
+plt.grid(zorder=0,alpha=0.4)
+
+plt.savefig('figures/avgAndNum.png', dpi=800)
+
+

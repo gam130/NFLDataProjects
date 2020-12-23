69692e51a7d7fa360976ec92c7377943bcaf1f89
Gautam Maybhate <gautam.maybhate@gmail.com>
2020-12-23 13:40:37 -0500

    added steelers EPA file

---
+++ scripts/steelersEPA.py
@@ -0,0 +1,31 @@
+import pandas as pd
+import matplotlib.pyplot as plt
+
+YEAR = 2020
+
+data = pd.read_csv('https://github.com/guga31bb/nflfastR-data/blob/master/data/' \
+                         'play_by_play_' + str(YEAR) + '.csv.gz?raw=True',
+                         compression='gzip', low_memory=False)
+                         
+data = data.loc[data.season_type=='REG']
+
+data = data.loc[(data.posteam=='PIT')]
+
+data = data.loc[data.play_type.isin(['no_play','pass','run'])]
+
+data = data.groupby('week', as_index=False)[['epa']].mean()
+
+print(data)
+
+plt.figure(figsize=(10,10))
+
+plt.plot(data.week, data.epa, color='blue', marker='o')
+
+plt.grid(zorder=0, alpha=0.4)
+
+plt.xlabel('Week')
+plt.ylabel('Offensive EPA per play')
+plt.title('Steelers\' Offensive EPA Timeline')
+
+plt.savefig('figures/steelersEPA.png', dpi=800)
+
